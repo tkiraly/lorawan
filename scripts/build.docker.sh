f9bc09ca2b06b978b32c8f6a3c1dfd3029bd471b
Tamás Király <tamas@loriot.io>
2018-07-22 21:48:20 +0200

    Remove unneccessary scripts

--- scripts/build.docker.sh
+++
@@ -1,8 +0,0 @@
-GITC=$(git rev-list --count HEAD)
-TAG=$(git describe --tags --abbrev=0)
-IFS='.' read -ra vers <<< "$TAG"
-MAJOR="${vers[0]}"
-MINOR="${vers[1]}"
-
-docker build -t registry.gitlab.com/loriot/nawarol:$MAJOR.$MINOR.$GITC .
-docker push registry.gitlab.com/loriot/nawarol:$MAJOR.$MINOR.$GITC
