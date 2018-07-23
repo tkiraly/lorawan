398ef0665a058e44006d1a2a62d625f912ce786c
Tamás Király <tamas@loriot.io>
2018-07-24 01:11:26 +0200

    Refactor DataDown

--- macpayload/down.go
+++ macpayload/down.go
@@ -8,6 +8,7 @@ import (
 	"github.com/tkiraly/lorawan/commands"
 	"github.com/tkiraly/lorawan/fhdr"
 	"github.com/tkiraly/lorawan/mhdr"
+	"github.com/tkiraly/lorawan/mic"
 )
 
 type DataDown interface {
@@ -19,42 +20,56 @@ type DataDown interface {
 	MIC() []byte
 }
 
-type dataDown []byte
+type dataDown struct {
+	bytes, nwkskey, appskey []byte
+}
 
 func (m dataDown) ByteArray() []byte {
-	return m
+	temp := make([]byte, len(m.bytes))
+	copy(temp, m.bytes)
+	if m.FPort() != nil {
+		key := m.appskey
+		if *m.FPort() == 0 {
+			key = m.nwkskey
+		}
+		fpay := endataMessage(m.FRMPayload(), false, m.DevAddr(), uint32(m.FCnt()), key)
+		start := int(1 + 7 + m.FOptsLen() + 1)
+		finish := len(m.bytes) - 4
+		copy(temp[start:finish], fpay)
+	}
+	return temp
 }
 
 func (m dataDown) MType() mhdr.MType {
-	v := mhdr.Parse(m[0])
+	v := mhdr.Parse(m.bytes[0])
 	return v.MType()
 }
 
 func (m dataDown) Major() mhdr.MajorVersion {
-	v := mhdr.Parse(m[0])
+	v := mhdr.Parse(m.bytes[0])
 	return v.Major()
 }
 
 func (d dataDown) FPort() *uint8 {
-	f := fhdr.ParseDown(d[1:])
+	f := fhdr.ParseDown(d.bytes[1:])
 	i := 1 + 7 + int(f.FOptsLen()) + 4
-	if len(d) > i {
-		return &d[1+7+f.FOptsLen()]
+	if len(d.bytes) > i {
+		return &d.bytes[1+7+f.FOptsLen()]
 	}
 	return nil
 }
 
 func (d dataDown) FRMPayload() []byte {
-	f := fhdr.ParseDown(d[1:])
+	f := fhdr.ParseDown(d.bytes[1:])
 	i := 1 + 7 + int(f.FOptsLen()) + 4
-	if len(d) > i {
-		return d[1+7+f.FOptsLen()+1 : len(d)-4]
+	if len(d.bytes) > i {
+		return d.bytes[1+7+f.FOptsLen()+1 : len(d.bytes)-4]
 	}
 	return nil
 }
 
 func (d dataDown) MIC() []byte {
-	return d[len(d)-4:]
+	return d.bytes[len(d.bytes)-4:]
 }
 
 func (du dataDown) String() string {
@@ -62,56 +77,90 @@ func (du dataDown) String() string {
 	if du.FPort() != nil {
 		fport = du.FPort()
 	}
-	return fmt.Sprintf("DataDown: MHDR: %s; FHDRUp: %s; FPort: %d; FRMPayload: %s; MIC: %s\n",
-		mhdr.Parse(du[0]),
-		fhdr.ParseDown(du[1:]),
+	return fmt.Sprintf("DataDown: MHDR: %s; FHDRUp: %s; FPort: %d; FRMPayload: %s; MIC: %s",
+		mhdr.Parse(du.bytes[0]),
+		fhdr.ParseDown(du.bytes[1:]),
 		*fport,
 		strings.ToUpper(hex.EncodeToString(du.FRMPayload())),
 		strings.ToUpper(hex.EncodeToString(du.MIC())))
 }
 func (du dataDown) DevAddr() []byte {
-	d := fhdr.ParseDown(du[1:])
+	d := fhdr.ParseDown(du.bytes[1:])
 	return d.DevAddr()
 }
 func (du dataDown) FCnt() uint16 {
-	d := fhdr.ParseDown(du[1:])
+	d := fhdr.ParseDown(du.bytes[1:])
 	return d.FCnt()
 }
 func (du dataDown) FOpts() []commands.Fopter {
-	d := fhdr.ParseDown(du[1:])
+	d := fhdr.ParseDown(du.bytes[1:])
 	return d.FOpts()
 }
 func (du dataDown) FPending() bool {
-	d := fhdr.ParseDown(du[1:])
+	d := fhdr.ParseDown(du.bytes[1:])
 	return d.FPending()
 }
 func (du dataDown) ADR() bool {
-	d := fhdr.ParseDown(du[1:])
+	d := fhdr.ParseDown(du.bytes[1:])
 	return d.ADR()
 }
 func (du dataDown) ACK() bool {
-	d := fhdr.ParseDown(du[1:])
+	d := fhdr.ParseDown(du.bytes[1:])
 	return d.ACK()
 }
 func (du dataDown) FOptsLen() uint8 {
-	d := fhdr.ParseDown(du[1:])
+	d := fhdr.ParseDown(du.bytes[1:])
 	return d.FOptsLen()
 }
 
 func NewDown(mtype mhdr.MType, major mhdr.MajorVersion, devaddr []byte,
 	fcnt uint16, fopts []commands.Fopter, adr,
 	ack, fpending bool, foptslen uint8, fport *uint8,
-	frmpayload, mic []byte) DataDown {
+	frmpayload, nwkskey, appskey []byte) DataDown {
 	m := make([]byte, 0)
 	m = append(m, mhdr.New(mtype, major).ByteArray()...)
 	m = append(m, fhdr.NewDown(devaddr, fcnt, fopts, adr,
 		ack, fpending, foptslen).ByteArray()...)
+	fstart := 0
+	ffinish := 0
+
+	fmt.Println(hex.EncodeToString(m))
 	if fport != nil {
+		//WARNING: MIC is calculated with encrypted FRMPayload
+		//but in the end FRMPayload must return decrypted value
 		m = append(m, *fport)
-		m = append(m, frmpayload...)
+
+		key := appskey
+		if *fport == 0 {
+			key = nwkskey
+		}
+		fstart = len(m)
+		ffinish = fstart + len(frmpayload)
+		fpay := endataMessage(frmpayload, false, devaddr, uint32(fcnt), key)
+		fmt.Println(hex.EncodeToString(fpay))
+		m = append(m, fpay...)
 	}
-	m = append(m, mic...)
-	return dataDown(m)
+	m = append(m, make([]byte, 4)...)
+	mi, err := mic.Calculate(m, nwkskey)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(hex.EncodeToString(m))
+	fmt.Println(hex.EncodeToString(mi))
+	copy(m[len(m)-4:], mi)
+
+	if fport != nil {
+		key := appskey
+		if *fport == 0 {
+			key = nwkskey
+		}
+		fpay := endataMessage(m[fstart:ffinish], false, devaddr, uint32(fcnt), key)
+		copy(m[fstart:ffinish], fpay)
+	}
+
+	return dataDown{bytes: m, nwkskey: nwkskey, appskey: appskey}
+
 }
 
 func ParseDown(p, nwkskey, appskey []byte) (DataDown, error) {
@@ -120,37 +169,16 @@ func ParseDown(p, nwkskey, appskey []byte) (DataDown, error) {
 	if len(p) < 12 {
 		return nil, fmt.Errorf("payload should have length at least 12: %d", len(p))
 	}
-	return dataDown(temp), nil
-}
-
-func ParseDownEncrypted(p, nwkskey, appskey []byte) (DataDown, error) {
-	temp := make([]byte, len(p))
-	copy(temp, p)
-	if len(p) < 12 {
-		return nil, fmt.Errorf("payload should have length at least 12: %d", len(p))
-	}
-	fheader := fhdr.ParseDown(p)
-
-	if len(p) > 1+7+int(fheader.FOptsLen())+4 { //have Fport
-		fport := uint8(p[1+7+fheader.FOptsLen()])
-		start := int(1 + 7 + fheader.FOptsLen() + 1)
-		finish := len(p) - 4
-
-		if fport == 0 {
-			if nwkskey != nil {
-				payload := encrypt(p[start:finish], false, fheader.DevAddr(), uint32(fheader.FCnt()), nwkskey)
-				copy(temp[start:finish], payload)
-			} else {
-				return nil, fmt.Errorf("nwkskey must be a valid key")
-			}
-		} else {
-			if appskey != nil {
-				payload := encrypt(p[start:finish], false, fheader.DevAddr(), uint32(fheader.FCnt()), appskey)
-				copy(temp[start:finish], payload)
-			} else {
-				return nil, fmt.Errorf("appskey must be a valid key")
-			}
+	du := dataDown{bytes: temp, nwkskey: nwkskey, appskey: appskey}
+	if du.FPort() != nil {
+		key := appskey
+		if *du.FPort() == 0 {
+			key = nwkskey
 		}
+		fpay := endataMessage(du.FRMPayload(), false, du.DevAddr(), uint32(du.FCnt()), key)
+		start := int(1 + 7 + du.FOptsLen() + 1)
+		finish := len(du.bytes) - 4
+		copy(du.bytes[start:finish], fpay)
 	}
-	return dataDown(temp), nil
+	return du, nil
 }
