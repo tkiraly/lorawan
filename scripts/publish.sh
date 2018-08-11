73e78b538ff259d78c5c286f4c835297b67002a2
Tamás Király <tamas@loriot.io>
2018-08-11 15:58:28 +0200

    Fix publish script when no parameter supplied

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,13 +5,16 @@ IFS='.' read -ra vers <<< "$TAG"
 MAJOR="${vers[0]}"
 MINOR="${vers[1]}"
 
-if [ $1 = "mi" ]; then
-    MINOR=$(($MINOR+1))
-fi
+if [ $# -eq 1 ]; then
+    if [ $1 = "mi" ]; then
+        MINOR=$(($MINOR+1))
+    fi
 
-if [ $1 = "ma" ]; then
-    MAJOR=$(($MAJOR+1))
+    if [ $1 = "ma" ]; then
+        MAJOR=$(($MAJOR+1))
+    fi
 fi
 
 git tag "$MAJOR.$MINOR.$GITC"
+git push
 git push --tags
