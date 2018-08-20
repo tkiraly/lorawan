3e218203ad2be0a3ecabc9a76e6d1ab243a90ee0
Tamás Király <tamas@loriot.io>
2018-08-20 17:21:24 +0200

    Add comments to devstatus subpackage

--- commands/devstatus/devstatus.go
+++ commands/devstatus/devstatus.go
@@ -6,13 +6,16 @@ import (
 	"github.com/tkiraly/lorawan/commands"
 )
 
+// Reqlen is the length of the request
 const Reqlen = 1
 
+// DevStatusReq requests for the status of the end-device
+// With the DevStatusReq command a network server
+// may request status information from an end-device.
 type DevStatusReq interface {
 	commands.Fopter
 }
 
-//DevStatusReq command, an end-device may validate its connectivity with the network.
 type devStatusReq []byte
 
 func (c devStatusReq) ByteArray() []byte {
@@ -27,25 +30,53 @@ func (c devStatusReq) String() string {
 	return fmt.Sprintf("%s!", "DevStatusReq")
 }
 
+// ParseReq parses the byte array representation of the request
 func ParseReq(bb []byte) commands.Fopter {
 	b := make([]byte, len(bb))
 	copy(b, bb)
 	return devStatusReq(b)
 }
 
+// NewReq creates a new request
 func NewReq() DevStatusReq {
 	return devStatusReq([]byte{commands.DevStatusReqCommand})
 }
 
+// Anslen is the length of the answer
 const Anslen = 3
 
+// DevStatusAns contains the status of the end-device
+// If a DevStatusReq is received by an end-device,
+// it responds with a DevStatusAns command.
+// +======================+=========+========+
+// |     Size (bytes)     |    1    |   1    |
+// +======================+=========+========+
+// | DevStatusAns Payload | Battery | Margin |
+// +----------------------+---------+--------+
 type DevStatusAns interface {
 	commands.Fopter
+	// +=========+================================================================+
+	// | Battery |                          Description                           |
+	// +=========+================================================================+
+	// |       0 | The end-device is connected to an external power source.       |
+	// +---------+----------------------------------------------------------------+
+	// | 1...254 | The battery level, 1 being at minimum and 254 being at maximum |
+	// +---------+----------------------------------------------------------------+
+	// |     255 | The end-device was not able to measure the battery level.      |
+	// +---------+----------------------------------------------------------------+
 	Battery() uint8
+	// The margin (Margin) is the demodulation signal-to-noise ratio in dB
+	// rounded to the nearest integer value for the last successfully received
+	// DevStatusReq command. It is a signed integer of 6 bits with a minimum
+	// value of -32 and a maximum value of 31.
+	// +========+=====+========+
+	// |  Bits  | 7:6 |  5:0   |
+	// +========+=====+========+
+	// | Status | RFU | Margin |
+	// +--------+-----+--------+
 	Margin() uint8
 }
 
-//DevStatusReq command, an end-device may validate its connectivity with the network.
 type devStatusAns []byte
 
 func (c devStatusAns) ByteArray() []byte {
@@ -68,12 +99,14 @@ func (c devStatusAns) String() string {
 	return fmt.Sprintf("%s! Battery: %d, Margin: %d", "DevStatusAns", c.Battery(), c.Margin())
 }
 
+// ParseAns parses the byte array representation of the answer
 func ParseAns(bb []byte) commands.Fopter {
 	b := make([]byte, len(bb))
 	copy(b, bb)
 	return devStatusAns(b)
 }
 
+// NewAns creates a new answer
 func NewAns(battery, margin uint8) DevStatusAns {
 	return devStatusAns([]byte{commands.DevStatusAnsCommand, battery, margin})
 }
